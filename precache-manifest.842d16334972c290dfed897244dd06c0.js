f0278d3c5a9acf3e5104b95b31e2aac66184aa70
Hieu Duong <hieu.duong@vietbrilliant.com>
2020-08-05 17:10:55 -0700

    Updates

---
+++ precache-manifest.842d16334972c290dfed897244dd06c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf756293405e646e7ed3dcec707e3094",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7b861dd403d726654746",
+    "url": "/react-monsters-rolodex/static/css/main.c5e28cb1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b861dd403d726654746",
+    "url": "/react-monsters-rolodex/static/js/main.add75253.chunk.js"
+  },
+  {
+    "revision": "84cac6f49c2db027c893",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.782a98e2.js"
+  }
+]);
